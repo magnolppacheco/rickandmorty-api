a0137f287fcd6ef4012eced70549b7391e690867
magnolppacheco <magnolppacheco@gmail.com>
2022-08-28 14:14:16 -0300

    feat: implement FindUserByIDUseCase

---
+++ src/services/usecases/findUserByID.js
@@ -0,0 +1,15 @@
+export class FindUserByIdUseCase {
+    constructor(userRepository) {
+        this.repository = userRepository;
+    };
+
+    async execute(userId) {
+        if (!userId){
+            throw new Error("User ID sended is not a valid!");
+        }
+         const userFinded = await this.userRepository.findById(userId);
+         return userFinded;
+    };
+
+
+};
