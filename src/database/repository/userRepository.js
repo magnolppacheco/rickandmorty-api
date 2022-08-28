2237c2d59d6e746fe39a743ebad5d8876ced0f6f
magnolppacheco <magnolppacheco@gmail.com>
2022-08-28 14:46:07 -0300

    feat: implement UserRepositoryRepository

---
+++ src/database/repository/userRepository.js
@@ -0,0 +1,17 @@
+export class UserRepositoryMongoDb {
+    async create(user){
+        return user;
+    }
+
+    async findById(id){
+        return id;
+    }
+
+    async updateUser(user){
+        return user;
+    }
+
+    async deleteUser(id){
+        return id;
+    }
+}
