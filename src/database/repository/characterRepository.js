afde599887d156230f6add5928359653c386618d
magnolppacheco <magnolppacheco@gmail.com>
2022-08-28 22:37:48 -0300

    feat: create characterRepository

---
+++ src/database/repository/characterRepository.js
@@ -0,0 +1,27 @@
+import {characterDb} from "../mongo/schemas/character.js";
+
+export class UserRepositoryMongoDb {
+    async create(character){
+        return await characterDb.create(character);
+    }
+
+    async findAll(){
+        return await characterDb.find();
+    }
+
+    async findByName(characterName){
+        return await characterDb.findOne({name: characterName});
+    }
+
+    async findById(characterId){
+        return await characterDb.findOne({id: characterId});
+    }
+
+    async update(character){
+        return await characterDb.findOneAndUpdate({id:character.id}, character, {new:true});
+    } 
+
+    async deleteUser(id){
+        return await userDb.findOneAndDelete(id);
+    }
+}
