1269ff19517819d450718d4b8710b5123d7dd1a4
magnolppacheco <magnolppacheco@gmail.com>
2022-08-28 22:13:11 -0300

    feat: create characterUseCases

---
+++ src/services/usecases/character/updateCharacter.js
@@ -0,0 +1,22 @@
+import { CharacterEntity } from "../../../entities/character";
+
+export class UpdateCharacterUseCase {
+    constructor(characterRepository, findCharacterById) {
+      this.repository = characterRepository;
+      this.findCharacterById = findCharacterById;
+    }
+
+    async execute(characterUpdated, characterId){
+        const findedCharacter = await this.findCharacterById.execute(characterId);
+
+        const updatedCharacter = Object.assign(findedCharacter, characterUpdated);
+
+        const validatedCharacter = new CharacterEntity(updatedCharacter);
+
+        validatedCharacter.validate();
+
+        return await this.repository.update(validatedCharacter.getCharacter());
+    }
+
+
+}
